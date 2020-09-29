dc85f2b593cf0aa59383f59144fd48f92c049d78
Christopher Mancini <cmm1156@nyu.edu>
2020-09-29 15:41:10 -0400

    Basic setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// Styling
+import "./App.css";
 
+// Containers
 import Home from "./containers/Home";
 
 function App() {
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,7 +1,37 @@
 import React from "react";
 
+import Header from "../components/Header";
+
+const weatherKey = `5b82567045514b74df6771aaa7d2a828`;
+
 function Home() {
-    return <h1>Weather App</h1>;
+  /*
+    Weather type
+    current temp
+    high temp
+    low temp
+    cloudiness
+    humidity
+    wind speed
+  */
+
+  return (
+    <>
+      <Header />
+      <main className="Home">
+        <h2>Weather in Seoul</h2>
+        <div className="WeatherInfo">
+          <p>Weather Type: Cloudy</p>
+          <p>Current Temperature: 100 deg</p>
+          <p>High Temp: 100 deg</p>
+          <p>Low Temp: 80 deg</p>
+          <p>Cloudiness: 100%</p>
+          <p>Humidity: 35%</p>
+          <p>Wind Speed: 3km/h</p>
+        </div>
+      </main>
+    </>
+  );
 }
 
 export default Home;
